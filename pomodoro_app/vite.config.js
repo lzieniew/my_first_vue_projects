e65d9f7d9eb57c871b74e163fbf4fbb3e3604c4b
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2024-05-11 14:00:38 +0200

    Deployment preparation

--- pomodoro_app/vite.config.js
+++ pomodoro_app/vite.config.js
@@ -3,8 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const base = process.env.NODE_ENV === 'production'
+  ? '/lzieniew/my_first_vue_projects/pomodoro/'
+  : '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [
     vue(),
   ],
